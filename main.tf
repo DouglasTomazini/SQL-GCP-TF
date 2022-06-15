687ea4c7e70844fa78246d0d7998046895b5c7ac
DouglasTomazini <douglastomazini@gmail.com>
2022-06-15 11:11:33 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "google_sql_database_instance" "mysql-from-tf" {
+  name = "mysql-from-tf"
+  deletion_protection = false
+  region = "us-central1"
+
+
+  settings{
+    tier = "db-f1-micro"
+  }
+}
+
+
+resource "google_sql_user" "myuser" {
+  name = "usuario"
+  password = "senha"
+  instance = google_sql_database_instance.mysql-from-tf.name
+}
