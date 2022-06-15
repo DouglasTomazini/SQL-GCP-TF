e21bf832468c32a11bfc8bfdf59bddd5d6fb40cb
DouglasTomazini <douglastomazini@gmail.com>
2022-06-15 11:11:59 -0300

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+        source = "hashicorp/google"
+        version = "3.85.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "ID-from-your-project"
+  region = "us-central1"
+  zone = "us-central1-a"
+}
